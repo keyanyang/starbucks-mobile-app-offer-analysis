d4e79d7cf6d69dc58a559eeaacb9533731ae84f8
github kyleyang <kxy156@case.edu>
2020-07-08 00:34:22 -0700

    feat: data clean and part of EDA

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,2 +1,35 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+
+
+def plot_offer_utilization(df_offer):
+    # distribution of offer utilization 
+    offer_use = df_offer.groupby(['person', 'is_offer_used']).count()['offer_id'].unstack().reset_index().fillna(0)
+    offer_use.columns = ['person', 'not_used', 'used']
+    offer_use['utilization'] = offer_use['used'] / (offer_use['not_used'] + offer_use['used'])
+    offer_use['utilization'].hist(bins=60)
+    plt.xlabel('Utilization')
+    plt.ylabel('Count')
+    plt.title('Offer Utilization Distribution')
+    plt.show()
+
+def plot_offer_utilization_by_group(df_offer, df_profile, group):
+    offer_use = df_offer.groupby(['person', 'is_offer_used']).count()['offer_id'].unstack().reset_index().fillna(0)
+    offer_use.columns = ['person', 'not_used', 'used']
+    offer_use['utilization'] = offer_use['used'] / (offer_use['not_used'] + offer_use['used'])
+
+    merged = offer_use.merge(df_profile, left_on='person', right_on='id')
+    merged = merged[[group, 'utilization']]
+    
+    vals, labels = [], []
+    print(merged[group].unique())
+    for g in np.sort(merged[group].unique()):
+        vals.append(np.array(merged.loc[merged[group] == g, 'utilization']))
+        labels.append(g)
+    plt.hist(vals, alpha=0.5, density=False, histtype='bar', stacked=True, label=labels, bins=20)
+
+    plt.xlabel('Utilization')
+    plt.ylabel('Count')
+    plt.title(f'Offer Utilization by {group} Distribution')
+    plt.legend()
+    plt.show()
